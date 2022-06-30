220c66810e2e1f7da75c6b30d9a725895ef5885d
YUMIN KIM <heyday2036@khu.ac.kr>
2022-06-30 21:08:37 +0900

    Create 1697 숨바꼭질.py

---
+++ bfs/1697 숨바꼭질.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+n, k = map(int,input().split())
+max = 100000
+time = [0]*(max+1)
+
+def bfs(n,k):
+    queue = deque()
+    queue.append(n) # 출발지점 
+    while queue:
+        x = queue.popleft()
+        if x == k: # 동생 위치 찾으면 종료
+            return time[x]
+        for nx in (x-1, x+1, 2*x): # 3가지 경우 추적       
+            if 0 <= nx <= max and not time[nx]: # 방문하지 않았고, 범위 안에 해당 될 때만 계산수행
+                time[nx] = time[x] + 1
+                queue.append(nx)          
+                
+time = bfs(n,k)
+print(time)
