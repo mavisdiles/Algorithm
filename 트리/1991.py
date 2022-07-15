2152e9051522448543062a5c26a0b09658cd140b
YUMIN KIM <heyday2036@khu.ac.kr>
2022-07-15 22:25:57 +0900

    Update 1991.py

--- 트리/1991.py
+++ 트리/1991.py
@@ -1,2 +1,51 @@
-n = map(int,input())
-nodes = [list(map(int, input().split())) for _ in range(n)]
+n = int(input())
+tree = {}
+pre_str =''
+in_str =''
+post_str =''
+class node:
+    def __init__(self,data,left,right):
+        self.data = data
+        self.left = left
+        self.right = right
+        
+for i in range(n):
+    a,b,c = input().split()
+    if b=='.':
+        b=None
+    if c=='.':
+        c=None
+    tree[a] = node(a,b,c)
+
+def preorder(root):
+    global pre_str
+    pre_str += root.data
+    if root.left:
+        preorder(tree[root.left])    
+    if root.right:
+        preorder(tree[root.right])
+
+def inorder(root): 
+    global in_str  
+    if root.left:
+        inorder(tree[root.left])  
+    in_str += root.data
+    if root.right:
+        inorder(tree[root.right])
+        
+def postorder(root):    
+    global post_str
+    if root.left:
+        postorder(tree[root.left])    
+    if root.right:
+        postorder(tree[root.right])
+    post_str += root.data
+
+head = tree['A']  
+
+preorder(head)
+print(pre_str)
+inorder(head)
+print(in_str)
+postorder(head)
+print(post_str)
