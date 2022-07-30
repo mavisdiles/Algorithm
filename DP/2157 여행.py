9ea0acb84600dcb5554ba0662adb4a009292352d
YUMIN KIM <heyday2036@khu.ac.kr>
2022-07-30 10:57:54 +0900

    save 2157.py

--- DP/2157 여행.py
+++ DP/2157 여행.py
@@ -2,10 +2,13 @@ import sys
 
 n,m,k = map(int,sys.stdin.readline().split())
 station = {}
-graph = [[False]*(n+1) for _ in range(n+1)]
-print(graph)
+graph = [[0]*(n+1) for _ in range(n+1)]
+
 for i in range(k):
     a,b,c = sys.stdin.readline().split()
     if a<b:
         station[a] = (b,c)
 
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        graph[i][j] = max()
--- 트라이/14426 접두사 찾기.py
+++ 트라이/14426 접두사 찾기.py
@@ -1,6 +1,19 @@
+#pypy 통과, 굳이 트라이?
 import sys
-n, m = sys.stdin.readline()
+n, m = map(int,sys.stdin.readline().split())
 s=[]
+
 for i in range(n):
-    str = sys.stdin.readline()
+    str = sys.stdin.readline().strip()
     s.append(str)
+
+count =0
+
+for i in range(m):
+    prefix = sys.stdin.readline().strip()
+    for target in s:
+        if prefix == target[:len(prefix)]:
+            count +=1
+            break
+
+print(count)
