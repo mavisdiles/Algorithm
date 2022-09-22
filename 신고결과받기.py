ea16f77e5ccbdcf727628bac9b9520515d60da2f
YUMIN KIM <heyday2036@khu.ac.kr>
2022-09-22 16:48:14 +0900

    write 신고결과

--- 신고결과받기.py
+++ 신고결과받기.py
@@ -1,23 +1,26 @@
 from collections import defaultdict
 def solution(id_list, report, k):
+
     target = defaultdict(set)
     user_dic = defaultdict(list)
+    
     for i in report:
-        a,b = i.split(" ")
+        a,b = i.split()
         target[b].add(a)
         user_dic[a].append(b) 
         
     banned_id_list =[]
     for i in id_list:
-        if len(target[i]) >= 2:
+        if len(target[i]) >= k:
             banned_id_list.append(i)
-         
+        
     report_count = defaultdict(int)
     for i in id_list:
+        report_count[i]=0
         for j in banned_id_list:
             if j in user_dic[i]:
                 report_count[i] += 1
             
-        
-    answer = report_count.values()
+    answer = list(report_count.values())
+    
     return answer
